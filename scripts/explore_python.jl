c3f40fcf27bd1579eac6c5b3752cd07ded67f145
rymanderson <rymanderson@gmail.com>
2022-11-21 20:48:25 -0700

    prep restart file creation script

--- scripts/explore_python.jl
+++ scripts/explore_python.jl
@@ -3,26 +3,27 @@ import FLOWPy as fp
 # prep restart file
 
 rpm = 1800.0
-nrevs = 1/5
+nrevs = 1/5 * 100
 duration = nrevs / (rpm/60)
 dt = 0.001 # approximately
 nsteps = Int(ceil(duration/dt))
 
-
-# nsteps = 10 # timesteps
-# duration = 0.01 # seconds
 vinf = 67.0 # m/s
 alpha_rad = 4.0 * 3.1415926/180.0 # aoa in radians
-# restart_vpmfile = nothing
-# vpmdata = fp.VPMData(nsteps, duration; restart_vpmfile)
+restart_vpmfile = nothing
+vpmdata = fp.VPMData(nsteps, duration; restart_vpmfile)
 
-# for _ in 0:nsteps
-#   fp.step_vpm(vpmdata, vinf, alpha_rad)
-# end
+for _ in 0:nsteps
+    fp.step_vpm(vpmdata, vinf, alpha_rad)
+end
 
-# continue stepping from restart file
+#####
+##### continue stepping from restart file
+#####
+#=
 restart_vpmfile = "second_run/FLOWUnsteadyPy_pfield.7.h5"
 vpmdata = fp.VPMData(nsteps, duration; restart_vpmfile)
 for _ in 0:nsteps
-  fp.step_vpm(vpmdata, vinf, alpha_rad)
+    fp.step_vpm(vpmdata, vinf, alpha_rad)
 end
+=#
