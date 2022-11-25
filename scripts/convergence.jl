4362812016b1b333a6ad3d45d1e365891f129af5
rymanderson <rymanderson@gmail.com>
2022-11-25 16:45:37 -0700

    update n in convergence scripts

--- scripts/convergence.jl
+++ scripts/convergence.jl
@@ -16,7 +16,7 @@ function run_ecrm2(;
     alpha_rad = alpha_deg * 3.1415926/180.0 # aoa in radians
     restart_vpmfile = nothing
     run_name="a$(Int(ceil(alpha_deg)))_dt$(round(dt; digits=3))_n$(n)_nr$(n_ccb)_rotors$(add_rotors)"
-    vpmdata = fp.VPMData(nsteps, duration; restart_vpmfile, add_rotors, run_name)
+    vpmdata = fp.VPMData(nsteps, duration; restart_vpmfile, add_rotors, run_name, n, n_ccb)
 
     # fp.update_ecrm_002(vpmdata, data_list)
 
