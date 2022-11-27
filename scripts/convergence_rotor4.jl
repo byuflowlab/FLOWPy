a16aa3f13c84790cb76b9051a94ae25ab90de1d6
rymanderson <rymanderson@gmail.com>
2022-11-26 23:26:41 -0700

    update rotor convergence script

--- scripts/convergence_rotor4.jl
+++ scripts/convergence_rotor4.jl
@@ -5,14 +5,6 @@ run_ecrm2(;
     alpha_deg = 6.0,
     dt = 0.001, # approximately
     add_rotors=true,
-    n=40, n_ccb=40,
-    nrevs=1/5*60
-)
-
-run_ecrm2(;
-    alpha_deg = 6.0,
-    dt = 0.001, # approximately
-    add_rotors=true,
     n=80, n_ccb=40,
     nrevs=1/5*60
 )
@@ -22,5 +14,13 @@ run_ecrm2(;
     dt = 0.001, # approximately
     add_rotors=true,
     n=80, n_ccb=20,
+    nrevs=1/5*60
+)
+
+run_ecrm2(;
+    alpha_deg = 6.0,
+    dt = 0.001, # approximately
+    add_rotors=true,
+    n=160, n_ccb=40,
     nrevs=1/5*60
 )
--- scripts/convergence_rotor5.jl
+++ scripts/convergence_rotor5.jl
@@ -16,3 +16,11 @@ run_ecrm2(;
     n=40, n_ccb=20,
     nrevs=1/5*60
 )
+
+run_ecrm2(;
+    alpha_deg = 6.0,
+    dt = 0.001, # approximately
+    add_rotors=true,
+    n=20, n_ccb=20,
+    nrevs=1/5*60
+)
