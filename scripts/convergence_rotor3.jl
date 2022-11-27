5f5d0258f6e619fed68b26aa4f49d8f2bbb534e8
rymanderson <rymanderson@gmail.com>
2022-11-26 23:12:50 -0700

    rotor n convergence

--- scripts/convergence_rotor3.jl
+++ scripts/convergence_rotor3.jl
@@ -1,11 +1,11 @@
 include("convergence.jl")
 
-# n
+# nr
 run_ecrm2(;
     alpha_deg = 6.0,
     dt = 0.001, # approximately
     add_rotors=true,
-    n=20, n_ccb=10,
+    n=20, n_ccb=20,
     nrevs=1/5*60
 )
 
@@ -13,6 +13,6 @@ run_ecrm2(;
     alpha_deg = 6.0,
     dt = 0.001, # approximately
     add_rotors=true,
-    n=40, n_ccb=10,
+    n=160, n_ccb=40,
     nrevs=1/5*60
 )
