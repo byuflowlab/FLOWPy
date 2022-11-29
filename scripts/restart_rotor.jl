1ae7459874fd6bde9644bcfc13b511a2e741b914
rymanderson <rymanderson@gmail.com>
2022-11-29 09:48:12 -0700

    add rotor restart script

---
+++ scripts/restart_rotor.jl
@@ -0,0 +1,12 @@
+include("restart.jl")
+
+run_restart(;
+        alpha_deg = 6.0,
+        dt = 0.001, # approximately
+        add_rotors=true,
+        n=20, n_ccb=20,
+        nrevs=1/5*2,
+        p_per_step=5,
+        restart_vpmfile="../restart/a6_dt0.001_n20_nr20_rotorstrue_pfield.300.h5",
+        restart_nsteps=300
+    )
