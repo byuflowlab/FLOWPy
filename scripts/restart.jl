435eb9c6a7ad2c82736c8f0c654c6f3bcff3b136
rymanderson <rymanderson@gmail.com>
2022-11-29 09:45:36 -0700

    fix restart syntax

--- scripts/restart.jl
+++ scripts/restart.jl
@@ -5,9 +5,10 @@ function run_restart(;
         dt = 0.001, # approximately
         add_rotors=true,
         n=20, n_ccb=20,
-        nrevs=1/5*5,
+        nrevs=1/5*2,
         p_per_step=5,
-        restart="../restart/a6_dt0.001_n20_nr20_rotorstrue_pfield.300.h5"
+        restart_vpmfile="../restart/a6_dt0.001_n20_nr20_rotorstrue_pfield.300.h5",
+        restart_nsteps=300
     )
     # prep restart file
     rpm = 1800.0
@@ -16,9 +17,8 @@ function run_restart(;
 
     vinf = 67.0 # m/s
     alpha_rad = alpha_deg * 3.1415926/180.0 # aoa in radians
-    restart_vpmfile = nothing
     run_name="restart_a$(Int(ceil(alpha_deg)))_dt$(round(dt; digits=3))_n$(n)_nr$(n_ccb)_rotors$(add_rotors)_pps$(p_per_step)"
-    vpmdata = fp.VPMData(nsteps, duration; restart_vpmfile, add_rotors, run_name, n, n_ccb, p_per_step)
+    vpmdata = fp.VPMData(nsteps, duration; restart_vpmfile, restart_nsteps, add_rotors, run_name, n, n_ccb, p_per_step)
 
     # fp.update_ecrm_002(vpmdata, data_list)
     println("Running $(run_name):")
