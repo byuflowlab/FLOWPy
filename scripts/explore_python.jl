0510698689e3561b638b66d2e9dc1e3991c2ac80
rymanderson <rymanderson@gmail.com>
2022-11-29 18:10:00 -0700

    update explore_python.jl

--- scripts/explore_python.jl
+++ scripts/explore_python.jl
@@ -3,7 +3,7 @@ using PyPlot
 # prep restart file
 
 rpm = 1800.0
-nrevs = 1/5 * 35 # 100
+nrevs = 1/5 # 100
 duration = nrevs / (rpm/60)
 dt = 0.001 # approximately
 nsteps = Int(ceil(duration/dt))
@@ -20,20 +20,3 @@ for i in 0:nsteps
     save_file = i == nsteps ? true : false
     fp.step_vpm(vpmdata, vinf, alpha_rad; save_file)
 end
-
-#####
-##### continue stepping from restart file
-#####
-#=
-restart_vpmfile = "second_run/FLOWUnsteadyPy_pfield.7.h5"
-vpmdata = fp.VPMData(nsteps, duration; restart_vpmfile)
-for _ in 0:nsteps
-    fp.step_vpm(vpmdata, vinf, alpha_rad)
-end
-=#
-
-
-
-directory = ".."
-filename = joinpath(directory, run_name * "_history.h5")
-include("plot.jl")
