257b9c8e17f7c2bd8f63a85bb694e79cb9309470
rymanderson <rymanderson@gmail.com>
2022-12-09 12:40:37 -0700

    update convergence script

--- scripts/convergence_rotor_time2.jl
+++ scripts/convergence_rotor_time2.jl
@@ -2,16 +2,7 @@ include("convergence.jl")
 
 run_ecrm2(;
     alpha_deg = 6.0,
-    dt = 0.00005, # approximately
-    add_rotors=true,
-    n=20, n_ccb=10,
-    nrevs=1/5*60,
-    p_per_step=1
-)
-
-run_ecrm2(;
-    alpha_deg = 6.0,
-    dt = 0.00002, # approximately
+    dt = 0.0005, # approximately
     add_rotors=true,
     n=20, n_ccb=10,
     nrevs=1/5*60,
