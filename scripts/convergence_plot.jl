fd786a5ca1feb05f1f1af0a50b0f2e9190d7a06c
rymanderson <rymanderson@gmail.com>
2022-12-09 10:12:04 -0700

    more convergence

--- scripts/convergence_plot.jl
+++ scripts/convergence_plot.jl
@@ -2,11 +2,8 @@ include("plot.jl")
 
 function history_tag(alpha, dt, n, nr, rotors, pps=5)
     alpha_tag = "a$(Int(round(alpha; digits=0)))"
-    get_digits = dt > 0.0009999 ? 3 : 4
+    get_digits = 5
     dt_tag = "_dt$(round(dt; digits=get_digits))"
-    if dt == 0.0005
-        dt_tag = "_dt0.0"
-    end
     n_tag = "_n$n"
     nr_tag = "_nr$nr"
     rotors_tag = "_rotors$rotors"
@@ -17,7 +14,7 @@ end
 #####
 ##### no rotors
 #####
-dt
+# dt
 fig_name = "norotors_dt"
 alpha = [6.0,6,6]
 dt = [0.002, 0.001, 0.0005]
@@ -26,7 +23,7 @@ nr = [10,10,10]
 rotors = [false, false, false]
 pps = [5,5,5]
 striders = [1,2,4]
-series_names = ["dt = $(round(dt[i]; digits=dt[i] > 0.0009999 ? 3 : 4))s" for i in 1:length(dt)]
+series_names = ["dt = $(round(dt[i]; digits=5))s" for i in 1:length(dt)]
 for i in 1:length(alpha)
     filename = history_tag(alpha[i], dt[i], n[i], nr[i], rotors[i])
     @show filename
@@ -55,6 +52,22 @@ end
 #####
 ##### rotors
 #####
+
+# dt
+fig_name = "rotors_dt"
+alpha = [6.0,6,6]
+dt = [0.002, 0.001, 0.0005]
+n = [20,20,20]
+nr = [20,20,20]
+rotors = [true, true, true]
+series_names = ["dt = $(round(dt[i]; digits=5))s" for i in 1:length(dt)]
+for i in 1:length(alpha)
+    filename = history_tag(alpha[i], dt[i], n[i], nr[i], rotors[i])
+    @show filename
+    hold = i == 1 ? false : true
+    make_legend = i == length(alpha) ? true : false
+    plot_this!(fig_name, series_names[i], "convergence_results/"*filename, dt[i], hold, make_legend; strider_cl=nothing, color_cl=(0.0,1-i/length(alpha), i/length(alpha)))
+end
 
 # n
 fig_name = "rotors_n"
---
+++ scripts/convergence_rotor_time2.jl
@@ -0,0 +1,23 @@
+include("convergence.jl")
+
+run_restart(;
+        alpha_deg = 6.0,
+        dt = 0.00005, # approximately
+        add_rotors=true,
+        n=20, n_ccb=10,
+        nrevs=1/5*60,
+        p_per_step=1,
+        restart_vpmfile="restart/a6_dt0.001_n20_nr20_rotorstrue_pfield.300.h5",
+        restart_nsteps=300
+    )
+
+run_restart(;
+        alpha_deg = 6.0,
+        dt = 0.00002, # approximately
+        add_rotors=true,
+        n=20, n_ccb=10,
+        nrevs=1/5*60,
+        p_per_step=1,
+        restart_vpmfile="restart/a6_dt0.001_n20_nr20_rotorstrue_pfield.300.h5",
+        restart_nsteps=300
+    )
